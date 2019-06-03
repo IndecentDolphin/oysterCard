8621a34220edab891cba6e7d5c17fd8adba67da0
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-03 14:41:19 +0100

    added tests for max limit being reached on card. Added code for this

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,15 @@ class OysterCard
     
     attr_reader :balance
     DEFAULT_BALANCE = 0.00
+    MAX_LIMIT = 90.00
+
     def initialize
         @balance = DEFAULT_BALANCE
     end
-
+    
+    def top_up(amount)
+        raise "Max balance reached" if balance == MAX_LIMIT
+        
+        @balance += amount
+    end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -6,4 +6,17 @@ describe OysterCard do
         expect(subject.balance).to eq(0.00)
     end
 
+    it "tops up card correctly to the input amount" do
+        expect(subject.top_up(10.00)).to eq(10.00)
+    end 
+   
+    # In order to protect my money from theft or loss
+    # As a customer
+    # I want a maximum limit (of £90) on my card
+
+    it "should throw an error if balance > 90.00" do
+        subject.top_up(90.00)
+        expect {subject.top_up(1.00)}.to raise_exception "Max balance reached"
+    end
+
 end
