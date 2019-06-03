ef2bd2f3c868988008074e2a9ca1043fdb2e7f07
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-03 16:00:06 +0100

    added tests to cover user tapping in and out, keeping an eye on their activity, code compliments tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,36 @@
 class OysterCard
     
     attr_reader :balance
-    DEFAULT_BALANCE = 0.00
-    MAX_LIMIT = 90.00
+
+    # constants
+    DEFAULT_BALANCE = 0
+    MAX_LIMIT = 90
+    
 
     def initialize
         @balance = DEFAULT_BALANCE
+        @card_state = false
     end
     
     def top_up(amount)
-        raise "Max balance reached" if balance == MAX_LIMIT
-        
+        raise "Max balance reached, balance: #{balance}, balance limit: #{MAX_LIMIT}" if balance == MAX_LIMIT
+
         @balance += amount
+    end
+
+    def deduct(fare)
+        @balance -= fare
+    end
+
+    def touch_in
+        @card_state = true
+    end
+
+    def touch_out
+        @card_state = false
+    end
+
+    def in_journey?
+        @card_state == true ? true : false
     end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,19 +4,30 @@ describe OysterCard do
 
     it "Checks balance of oyster card" do
         expect(subject.balance).to eq(0.00)
+        # subject.balance = 10
+        subject.top_up(10)
+        expect(subject.balance).to eq(10)
     end
 
     it "tops up card correctly to the input amount" do
         expect(subject.top_up(10.00)).to eq(10.00)
     end 
-   
-    # In order to protect my money from theft or loss
-    # As a customer
-    # I want a maximum limit (of £90) on my card
 
     it "should throw an error if balance > 90.00" do
-        subject.top_up(90.00)
-        expect {subject.top_up(1.00)}.to raise_exception "Max balance reached"
+        subject.top_up(90)
+        expect {subject.top_up(1)}.to raise_exception "Max balance reached, balance: 90, balance limit: 90"
+    end
+    
+    it "removes fare from card balance" do
+        subject.top_up(10)
+        expect(subject.deduct(3)).to eq(7)
+    end
+
+    it "starts and ends a user journey" do
+        subject.touch_in 
+        expect(subject).to be_in_journey
+        subject.touch_out
+        expect(subject).not_to be_in_journey
     end
 
 end
